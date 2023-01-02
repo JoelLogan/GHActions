7bb90ba5d1b11ba40b9f5b59c260bbeee7258eca
PercentBoat4164 <51725924+PercentBoat4164@users.noreply.github.com>
2023-01-02 02:01:30 -0500

    Update UpdateCheck.py

--- UpdateCheck.py
+++ UpdateCheck.py
@@ -5,26 +5,33 @@ ubuntu_codename = "" # Use this to override the ubuntu codename.
 
 # Get most recent Vulkan version.
 
+valid_tags = []
+for i in [i.split('/')[-1] for i in os.popen("git ls-remote --tags https://github.com/KhronosGroup/Vulkan-Headers.git").read().split()[1::2]]:
+	if i[0] == 's' and i[-1].isdigit():
+		valid_tags.append(i[4:-2])
 if not version:
 	print("Using latest Vulkan version: v", end="")
-	tags = [i.split('/')[-1] for i in os.popen("git ls-remote --tags https://github.com/KhronosGroup/Vulkan-Headers.git").read().split()[1::2]]
-	for i in tags:
-		if i[0] == 's' and i[-1].isdigit():
-			new_version = i[4:-2]
-			if new_version > version:
-				version = new_version
+	for i in valid_tags:
+		if i > version:
+			version = i
 else:
+	if version not in valid_tags:
+		raise Exception("Vulkan version: Overriden Vulkan version is not valid. Please select a valid Vulkan version. Valid versions:\n" + str(valid_tags))
 	print("Using version override: v", end="")
 print(version)
 
+# Get Ubuntu Codename
+
 if not ubuntu_codename:
-	print("Using current Ubuntu codename: ", end="")
 	if os.path.exists("/etc/os-release"):
-		ubuntu_codename = os.popen(". /etc/os-release; echo ${VERSION_CODENAME/*, /}").read()
-	elif os.path.exists("/etc/lsb-release"):
-		ubuntu_codename = os.popen(". /etc/lsb-release; echo ${VERSION_CODENAME/*, /}").read()
+		file = "/etc/os-release"
+	if os.path.exists("/etc/lsb-release"):
+		file = "/etc/lsb-release"
 	else:
-		print("ERROR: Ubuntu codename not found. Please verify that this script is executing in an Ubuntu environment. If so, provide a codename override.")
+		raise Excpetion("Ubuntu codename: Ubuntu codename not found. Please verify that this script is executing in an Ubuntu environment. If so, provide a codename override.")
+	print("Using current Ubuntu codename: ", end="")
+	with open(file, "r") as f:
+		ubuntu_codename = f.read().split("CODENAME=")[-1].split("\n")[0]
 else:
 	print("Using Ubuntu codename override: ", end="")
 print(ubuntu_codename)
