721fb3c1d1c6e9b3eed701c5e901e49a09c5c9fa
Joel Logan <76826688+JoelLogan@users.noreply.github.com>
2023-01-02 00:11:11 -0500

    Update UpdateCheck.py

--- UpdateCheck.py
+++ UpdateCheck.py
@@ -1,56 +1,21 @@
-import os,shutil,yaml
+import os
 
-actionlocation = '/.github/workflows'
-actionnames = ['testbuild.yml']
+version = "" # Use this to override the vulkan version.
 
-home = os.getcwd()
+# Get most recent Vulkan version.
 
-os.makedirs(home + '/vulkan')
-print('Made /vulkan')
-os.chdir(home + '/vulkan')
-print('Current Directory: ' + home)
-os.popen('git clone https://github.com/KhronosGroup/Vulkan-Headers.git').read()
-print('Vulkan Headers repo cloned')
-os.chdir(home + '/vulkan/Vulkan-Headers')
-print('Current Directory: ' + os.getcwd())
+if not version:
+	print("Using latest Vulkan version.")
+	tags = [i.split('/')[-1] for i in os.popen("git ls-remote --tags https://github.com/KhronosGroup/Vulkan-Headers.git").read().split()[1::2]]
+	for i in tags:
+		if i[0] == 's' and i[-1].isdigit():
+			new_version = i[4:-1]
+			if new_version > version:
+				version = new_version
+				
+else:
+	print("Using version override!")
 
-version = os.popen('git describe --tags --abbrev=0').read().lstrip('v')
-
-print("Latest version: " + version)
-
-os.chdir(home)
-shutil.rmtree("vulkan")
-
-os.chdir(home + actionlocation)
-
-for action in actionnames:
-    checkedversion = ""
-    actionfile = open(os.getcwd() + '/' + action)
-    actionfileyaml = yaml.safe_load(actionfile)
-    print("Loaded action file")
-    for key, value in actionfileyaml.items():
-        if "VULKAN_VERSION" in value:
-            checkedversion = str(value)
-            print("Found VULKAN_VERSION: "+ checkedversion[20:27])
-            if (checkedversion[20:27] != version):
-                actionfileyaml['env'] = {'VULKAN_VERSION': version}
-                actionfile.close()
-                print("Modified VULKAN_VERSION")
-                with open(os.getcwd() + '/' + action, 'w') as modifiedactionfile:
-                    yaml.dump(actionfileyaml, modifiedactionfile, sort_keys=False, default_flow_style=False)
-                    modifiedactionfile.close()
-                    print("Saved VULKAN_VERSION")
-                modifiedfileread = open(os.getcwd() + '/' + action, 'r')
-                fixedlines = ""
-                for line in modifiedfileread:
-                    new_line = line.replace('true:', 'on:')
-                    fixedlines = fixedlines + new_line
-                modifiedfilewrite = open(os.getcwd() + '/' + action, 'w')
-                modifiedfilewrite.write(fixedlines)
-                modifiedfilewrite.close()
-                print("Fixed True to on")
-
-
-print("Finished")
-                
-
+# Install the most recent Vulkan version.
+print("Installing Vulkan v" + str(version))
+os.system("sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test;wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -;wget -qO /etc/apt/sources.list.d/lunarg-vulkan-" + str(version) + "-focal.list https://packages.lunarg.com/vulkan/" + str(version) + "/lunarg-vulkan-" + str(version) + "-focal.list;sudo apt install vulkan-sdk")
